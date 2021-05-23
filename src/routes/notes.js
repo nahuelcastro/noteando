cafa51c9de8ab63c44eb69386bd4068682ba52e8
Nahuel <>
2021-05-23 02:23:50 -0300

    add note, view all notes

--- src/routes/notes.js
+++ src/routes/notes.js
@@ -1,8 +1,39 @@
 const express = require('express');
 const router = express.Router();
+const Note = require ('../models/Note');
 
-router.get('/notes', (req, res) => {
-    res.send('Notes from database');
+router.get('/notes/add', (req, res) => {
+    res.render('notes/new-notes');
 });
+
+router.post('/notes/new-note', async (req, res) => {
+    const {title, description} = req.body;
+    const errors = [];
+    if (!title){
+        errors.push({text: 'Please write a title'});
+    }
+    if (!description){
+        errors.push({text: 'Please write a description'});
+    }
+    if (errors.length > 0){
+        res.render('notes/new-notes', {
+            errors,
+            title,
+            description
+        })
+    } else {
+        const newNote = new Note({title, description});
+        await newNote.save();
+        res.redirect('/notes')
+    }
+})
+
+
+router.get('/notes', async (req, res) => {
+
+    const notes = await Note.find().lean();
+    res.render('notes/all-notes', {notes});
+});
+
 
 module.exports = router;
