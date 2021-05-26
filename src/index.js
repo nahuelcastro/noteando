820557f9955dbe2f07571189a940db77b267f711
Nahuel <>
2021-05-26 02:37:00 -0300

    log in finished

--- src/index.js
+++ src/index.js
@@ -6,11 +6,14 @@ const session = require('express-session');
 const dotenv = require('dotenv')
 const connectDB = require('./config/db')
 const flash = require('connect-flash');
+const passport = require('passport');
 
 // Initializations
 dotenv.config({ path: './src/config/config.env' })
 connectDB();
 const app = express();
+require('./config/passport');
+
 
 // Settings
 app.set('port', process.env.PORT || 3000);
@@ -31,13 +34,17 @@ app.use(session({
     resave: true,
     saveUninitialized: true
 }));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(flash());
+const aa = 8;
 
 
 // Global Variables
 app.use(((req, res, next) => {
     res.locals.success_msg = req.flash('success_msg');
     res.locals.error_msg = req.flash('error_msg');
+    res.locals.error = req.flash('error');
     next()
 }));
 
