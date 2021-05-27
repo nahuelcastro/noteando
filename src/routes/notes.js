0c06043c33d5f3b0415143aae7d3a06e427e52b4
Nahuel <>
2021-05-27 04:08:12 -0300

    only notes by user, finished basic app

--- src/routes/notes.js
+++ src/routes/notes.js
@@ -24,6 +24,7 @@ router.post('/notes/new-note', isAuthenticated, async (req, res) => {
         })
     } else {
         const newNote = new Note({title, description});
+        newNote.user = req.user.id;
         await newNote.save();
         req.flash('success_msg', 'Note Added Successfully')
         res.redirect('/notes')
@@ -54,7 +55,7 @@ router.delete('/notes/delete/:id', isAuthenticated, async (req, res) => {
 
 router.get('/notes', isAuthenticated,  async (req, res) => {
 
-    const notes = await Note.find().sort({date: 'desc'}).lean();
+    const notes = await Note.find({user: req.user.id}).sort({date: 'desc'}).lean();
     res.render('notes/all-notes', {notes});
 });
 
