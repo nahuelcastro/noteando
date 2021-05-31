15b27a50362722e779f56ce08437647b5ac61f72
Nahuel <>
2021-05-31 06:19:41 -0300

    pinned and tag

--- src/models/Note.js
+++ src/models/Note.js
@@ -4,6 +4,8 @@ const {Schema} = mongoose;
 const NoteSchema = new Schema({
     title: {type: String, required: true},
     description: {type: String, required: true},
+    tag: {type: String, required: false},
+    pinned: {type: Boolean, required: false},
     date: {type: Date, default: Date.now},
     user: {type: String}
 });
--- src/routes/notes.js
+++ src/routes/notes.js
@@ -8,7 +8,8 @@ router.get('/notes/add', isAuthenticated, (req, res) => {
 });
 
 router.post('/notes/new-note', isAuthenticated, async (req, res) => {
-    const {title, description} = req.body;
+    const {title, description, tag} = req.body;
+    let {pinned} = req.body;
     const errors = [];
     if (!title){
         errors.push({text: 'Please write a title'});
@@ -23,7 +24,8 @@ router.post('/notes/new-note', isAuthenticated, async (req, res) => {
             description
         })
     } else {
-        const newNote = new Note({title, description});
+        pinned = (pinned === undefined) ? pinned = false : pinned = true;
+        const newNote = new Note({title, description, tag, pinned});
         newNote.user = req.user.id;
         await newNote.save();
         req.flash('success_msg', 'Note Added Successfully')
@@ -35,6 +37,18 @@ router.post('/notes/new-note', isAuthenticated, async (req, res) => {
 router.get('/notes/edit/:id', isAuthenticated, async (req, res) => {
     const note = await Note.findById(req.params.id).lean();
     res.render('notes/edit-note', {note});
+});
+
+router.get('/notes/edit/nopin/:id', isAuthenticated, async (req, res) => {
+    const pinned = false
+    await Note.findByIdAndUpdate(req.params.id, {pinned});
+    res.redirect('/notes');
+});
+
+router.get('/notes/edit/pin/:id', isAuthenticated, async (req, res) => {
+    const pinned = true
+    await Note.findByIdAndUpdate(req.params.id, {pinned});
+    res.redirect('/notes');
 });
 
 
@@ -54,8 +68,9 @@ router.delete('/notes/delete/:id', isAuthenticated, async (req, res) => {
 
 
 router.get('/notes', isAuthenticated,  async (req, res) => {
-    const notes = await Note.find({user: req.user.id}).sort({date: 'desc'}).lean();
-    res.render('notes/all-notes', {notes});
+    const notes_pinned = await Note.find({user: req.user.id}).find({pinned: true}).sort({date: 'desc'}).lean();
+    const notes = await Note.find({user: req.user.id}).find({pinned: false}).sort({date: 'desc'}).lean();
+    res.render('notes/all-notes', {notes_pinned, notes});
 });
 
 
